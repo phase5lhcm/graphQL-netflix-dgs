f6a54fe0ef85e83ed8fafc1b52988b849b2ee61f
Christine Maynard <christine.ri.maynard@gmail.com>
2022-04-06 21:54:52 -0400

    First commit

---
+++ src/test/java/com/graphQLNetflix/graphQLNetflix/GraphQlNetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphQLNetflix.graphQLNetflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlNetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
